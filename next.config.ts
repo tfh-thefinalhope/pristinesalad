a0a1abccb9d9b16b382f5af7683321aae57cd2fb
TFH <tfhthefinalhope@gmail.com>
2025-12-30 19:29:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // ✅ REQUIRED for frontend-only hosting
+
   images: {
+    unoptimized: true, // ✅ REQUIRED for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'saladocafe.com',
+        protocol: "https",
+        hostname: "saladocafe.com",
       },
     ],
   },
 };
 
 export default nextConfig;
+
